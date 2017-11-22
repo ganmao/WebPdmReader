5dbdda7b3ad0d9ae8dcf7d0d315cfe7a355b546b
dongliang <zdl0812@163.com>
2017-11-22 17:30:31 +0800

    Create showPdmCol.go

---
+++ controllers/showPdmCol.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"WPdmReader/lib"
+
+	"github.com/astaxie/beego"
+)
+
+type ShowPdmController struct {
+	beego.Controller
+}
+
+func (this *ShowPdmController) Get() {
+	// 获取查询pdm文件列表
+	pdmPath, err := beego.GetConfig("String", "PdmPath", "data/pdm")
+	if err == nil {
+		beego.Debug("GET PdmPath : ", pdmPath)
+	}
+	WRPLibs.Check(err)
+
+	fList, err := WRPLibs.ListDir(pdmPath.(string), "pdm")
+	WRPLibs.Check(err)
+
+	// 数据赋值
+	// --- contents
+	this.Data["fileList"] = fList
+
+	// --- page header
+	this.Data["pageHeader"] = "Show PDM List"
+	this.Data["pDescription"] = "当前目录下的pdm文件"
+	this.Data["pLevel"] = "HOME"
+	this.Data["pLevelLink"] = "/"
+	this.Data["pHere"] = "PDMLIST"
+
+	// 界面展示
+	// 模板
+	this.Layout = "layout.html"
+	// 主要展示内容
+	this.TplName = "showPdmList.tpl"
+
+	// 子模板
+	this.LayoutSections = make(map[string]string)
+	// 页头
+	this.LayoutSections["pageHeader"] = "pageHeader.tpl"
+	// 当前页使用的script脚本
+	this.LayoutSections["userScripts"] = "dataTables_script.tpl"
+}
